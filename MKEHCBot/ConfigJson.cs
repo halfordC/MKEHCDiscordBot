04cf42e74a76d64bdf6303163ec5fe79fdddd4f9
halfordC <wcrossno@uwm.edu>
2023-01-28 12:50:20 -0600

    initial commit of bot template

---
+++ MKEHCBot/ConfigJson.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MKEHCBot
+{
+    internal class ConfigJson
+    {
+        [JsonProperty("token")]
+        public string Token { get; private set; }
+        [JsonProperty("prefix")]
+        public string Prefix { get; private set; }
+    }
+}
---
+++ MKEHCBot/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+
+namespace MKEHCBot // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var bot = new Bot();
+            bot.RunAsync().GetAwaiter().GetResult();
+        }
+
+    }
+}
